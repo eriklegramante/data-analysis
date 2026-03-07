55d6d9e49400b13e0374bfc4db3a71afeef02ab6
eriklegramante <eriklegramante@gmail.com>
2026-03-07 16:41:23 -0300

    Remove unnecessary comments in load.py for improved code clarity

--- scripts/load.py
+++ scripts/load.py
@@ -3,13 +3,11 @@ from sqlalchemy import create_engine
 
 class DataLoader:
     def __init__(self):
-        # Define o caminho para o arquivo do banco de dados na raiz do projeto
         self.db_url = "sqlite:///student_wellbeing.db"
         self.engine = create_engine(self.db_url)
 
     def load_to_sql(self, df, table_name):
         try:
-            # Salva os dados no arquivo local
             df.to_sql(table_name, con=self.engine, if_exists='replace', index=False)
             print(f"✅ Sucesso! Dados salvos no SQLite (student_wellbeing.db)")
         except Exception as e:
