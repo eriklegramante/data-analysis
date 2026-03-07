2bef54c8e8c0677e1c1f3bd098d211e24dbf8e43
eriklegramante <eriklegramante@gmail.com>
2026-03-07 15:58:06 -0300

    Refactor DataLoader to use environment variables for database connection and update success/error messages

--- scripts/load.py
+++ scripts/load.py
@@ -5,16 +5,17 @@ class DataLoader:
     def __init__(self):
         user = os.getenv("POSTGRES_USER")
         password = os.getenv("POSTGRES_PASSWORD")
-        host = "127.0.0.1"  
-        port = "5432"
-        db_name = "student_wellbeing_db"
+        host = os.getenv("POSTGRES_HOST") 
+        port = os.getenv("POSTGRES_PORT", "5432")
+        db_name = os.getenv("POSTGRES_DB", "postgres")
         
-        self.db_url = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
-        self.engine = create_engine(self.db_url)
+        self.db_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
+        
+        self.engine = create_engine(self.db_url, pool_pre_ping=True)
 
     def load_to_sql(self, df, table_name):
         try:
             df.to_sql(table_name, con=self.engine, if_exists='replace', index=False)
-            print(f"Dados carregados no PostgreSQL com sucesso na tabela {table_name}")
+            print(f"✅ Sucesso! Dados carregados no Supabase na tabela: {table_name}")
         except Exception as e:
-            print(f"Erro ao carregar no Postgres: {e}")
+            print(f"❌ Erro ao carregar no Supabase: {e}")
